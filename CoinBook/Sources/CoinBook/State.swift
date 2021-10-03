3b371875106ec2ce2cb65fe21b480ed0f8672f1a
eonil <pub.dev@eonil.com>
2021-10-03 15:47:25 +0900

    make some core types.

--- CoinBook/Sources/CoinBook/State.swift
+++ CoinBook/Sources/CoinBook/State.swift
@@ -1,12 +1,43 @@
 import Foundation
 
 struct State {
-
+    var orders = [Order]()
+    var trades = [Trade]()
+    struct Order {
+        
+    }
+    struct Trade {
+        
+    }
 }
+
+
 /// User command/intention.
 /// This is empty now as this app does not have controllable UI.
 enum Action {
 }
+
 enum Rendition {
     case state(State)
 }
+struct DataRendition {
+    var orderBook: OrderBook
+    struct OrderBook {
+        var items = [Item]()
+        struct Item {
+            var price: Double
+            var quantity: Double
+            var total: Double
+        }
+    }
+    var recentTrade: RecentTrade
+    struct RecentTrade {
+        var items = [Item]()
+        struct Item {
+            var price: Double
+            var quantity: Double
+            var time: Date
+        }
+    }
+}
+
